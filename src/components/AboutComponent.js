790786b8bc522eb5bd1fd08e77aba8181dbb7e25
Adityan <mail2adityan@gmail.com>
2021-07-30 16:52:44 +0530

    Adding simple animation to the leaders list in AboutComponent

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,20 +3,23 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderLeader({leader}) {
   return (
     <div className="col-12 mt-5">
-      <Media>
-          <Media left middle>
-            <Media object src={baseUrl + leader.image} alt={leader.name} />
-          </Media>
-          <Media body>
-            <Media heading>{leader.name}</Media>
-            <p>{leader.designation}</p>
-            <p>{leader.description}</p>
-          </Media>
-      </Media>
+      <FadeTransform in>
+        <Media>
+            <Media left middle>
+              <Media object src={baseUrl + leader.image} alt={leader.name} />
+            </Media>
+            <Media body>
+              <Media heading>{leader.name}</Media>
+              <p>{leader.designation}</p>
+              <p>{leader.description}</p>
+            </Media>
+        </Media>
+      </FadeTransform>
     </div>
   );
 }
@@ -25,7 +28,9 @@ function About(props) {
 
     const leaders = props.leaders.leaders.map((leader) => {
         return (
+          <Fade in>
             <RenderLeader leader={leader} />
+          </Fade>
         );
     });
 
@@ -104,7 +109,9 @@ function About(props) {
                   </div>
                   <div className="col-12">
                       <Media list>
+                        <Stagger in>
                           {leaders}
+                        </Stagger>
                       </Media>
                   </div>
               </div>
